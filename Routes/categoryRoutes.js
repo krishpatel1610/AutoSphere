0e71f416ada109e9bab809f6e2ed3788b974d43d
Krish Patel <115217085+krishpatel1610@users.noreply.github.com>
2024-06-25 16:23:34 +0530

    Update categoryRoutes.js

--- Routes/categoryRoutes.js
+++ Routes/categoryRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const categoryController = require('../controllers/categoryController');
+const categoryController = require('../Controllers/categoryController');
 
 // Get all categories
 router.get('/', categoryController.getCategories);

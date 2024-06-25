0176aa5208e49a856c8ee76d9f7445aece195750
Krish Patel <115217085+krishpatel1610@users.noreply.github.com>
2024-06-25 17:30:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,18 @@ const cityPriceRoutes = require('./Routes/cityPriceRoutes');
 
 mongoDB();
 // fcccccc
-app.use(cors());
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');

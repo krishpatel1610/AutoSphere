f5876343bdb8dc2f2063df4867431a76e56d7fbd
Krish Patel <115217085+krishpatel1610@users.noreply.github.com>
2024-06-25 17:39:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const mongoDB = require('./db');
 require('dotenv').config();
-const authRoutes = require('./Routes/Auths'); 
+const authRoutes = require('./Routes/Auths');
 const brandRoutes = require('./Routes/brandRoutes');
 const categoryRoutes = require('./Routes/categoryRoutes');
 const vehicleRoutes = require('./Routes/vehicleRoutes');
@@ -15,7 +15,7 @@ const allowedOrigins = [
 ];
 
 mongoDB();
-// fcccccc
+
 app.use(cors({
   origin: function (origin, callback) {
     // Allow requests with no origin (like mobile apps or curl requests)
@@ -29,28 +29,18 @@ app.use(cors({
   }
 }));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('hello kp!');
 });
 
-app.use(express.json());
-
-
 app.use('/api', authRoutes);
 app.use('/api/brands', brandRoutes);
-app.use('/api/categories', categoryRoutes); 
+app.use('/api/categories', categoryRoutes);
 app.use('/api/vehicles', vehicleRoutes);
 app.use('/api/cityprices', cityPriceRoutes);
 
 app.listen(port, () => {
-  console.log(`example is running on port ${port}`);
+  console.log(`Example app is running on port ${port}`);
 });

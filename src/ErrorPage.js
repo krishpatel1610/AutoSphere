3d3d3bc11ebbbe1ede3892005fdc6eff9010fefe
Krish Patel <115217085+krishpatel1610@users.noreply.github.com>
2024-06-25 21:00:02 +0530

    Update ErrorPage.js

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,6 +1,4 @@
-// Admin/ErrorPage.jsx
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '@mui/material';
 import { Result } from 'antd';
 import { useNavigate, useLocation } from 'react-router-dom';
@@ -8,6 +6,22 @@ import { useNavigate, useLocation } from 'react-router-dom';
 const ErrorPage = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const [isCompatible, setIsCompatible] = useState(window.innerWidth > 756);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsCompatible(window.innerWidth > 756);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Check initial window size
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const handleBackToHome = () => {
     const path = location.pathname;
@@ -17,6 +31,23 @@ const ErrorPage = () => {
       navigate('/');
     }
   };
+
+  if (!isCompatible && location.pathname.startsWith('/Admin')) {
+    return (
+      <div className="error-container" style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#f5f5f5' }}>
+        <Result
+          status="403"
+          title="Device Not Compatible"
+          subTitle="Sorry, your device is not compatible to access Admin functionality. Please use a larger screen."
+          extra={
+            <Button variant="contained" color="primary" onClick={handleBackToHome} style={{ textDecoration: 'none' }}>
+              Back to Home
+            </Button>
+          }
+        />
+      </div>
+    );
+  }
 
   return (
     <div className="error-container" style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#f5f5f5' }}>

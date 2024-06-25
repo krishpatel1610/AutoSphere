19501e63d8b975f018fe68e4046210d155445906
krishpatel1610 <115217085+krishpatel1610@users.noreply.github.com>
2024-06-25 16:06:29 +0530

    commit

--- src/Admin/redux/Store.js
+++
@@ -1,9 +0,0 @@
-// redux/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './Reducer';
-
-const store = configureStore({
-  reducer: rootReducer
-});
-
-export default store;

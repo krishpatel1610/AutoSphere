07e9ab3b4d77aa4f7f5834f8186e911d7f34ca52
krishpatel1610 <115217085+krishpatel1610@users.noreply.github.com>
2024-06-25 16:07:39 +0530

    commit

--- backend/Routes/cityPriceRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const cityPriceController = require('../controllers/cityPriceController');
-
-// Create a new city price
-router.post('/', cityPriceController.createCityPrice);
-
-module.exports = router;
